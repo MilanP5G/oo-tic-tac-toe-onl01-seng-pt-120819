2d9af5954de7284d7110d100b68e68d1bae1d105
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-01-15 17:00:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -105,7 +105,7 @@ class TicTacToe
     end 
     
     if won?
-      puts "Congratulations #{current_player}!"
+      puts "Congratulations #{!current_player}!"
     else draw?
       puts "It's A Tie!"
     end 
